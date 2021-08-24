76084219796b70f3ecf05fa7a105c664f5fa04eb
Miguel Puerta <migtarx@gmail.com>
2021-08-24 20:05:51 +0200

    Important changes
    Removed logs folder (Future feature) and all related code inside launching js files | Added error handling into Map rendering and file copying processes |
    Optimized and simplified method of copying new assets to the render folder

--- exec_render.js
+++ exec_render.js
@@ -4,20 +4,33 @@ const config = require('./config.json');
 
 console.log(`Minecraft Auto Overviewer Ready!`);
 console.log('New Render Started!'.green);
-console.log(config.render_out_dir)
-try {
-  shell.exec(`python${config.python_ver} ${config.minecraft_overviewer_loc} --config=${config.minecraft_overviewer_configfile_loc}`);
-} catch (error) {
-  console.log(`Error. Render Failed`.red);
-  console.log(`Saving error into log file`);
-  console.log(`Exiting Minecraft Auto Overviewer...`);
+
+var render_shell_out = shell.exec(`python${config.python_ver} ${config.minecraft_overviewer_loc} --config=${config.minecraft_overviewer_configfile_loc}`);
+var renderResult = (render_shell_out.substring(render_shell_out.length - 28, render_shell_out.length)).replace(/\s+/g, '');
+
+if (renderResult != "openindex.htmltoviewit.") {
+  console.log(`Error Minecraft Overviewer Render Failed`.red);
+  ErrorExit()
+} else {
+  console.log('Render complete!'.green);
+}
+
+console.log('Copying new assets'.yellow);
+for (var i = 0; i < config.assets.length; i++) {
+  try {
+    shell.cp(`./assets/${assets[i]}`,`${config.render_out_dir}`);
+  } catch (error) {
+    console.log('Error while copying assets to render folder'.red);
+    ErrorExit()
+  }
+  console.log(` => ${config.assets[i]} copied to render folder.`.cyan);
+}
+console.log('New Assets Copied'.green);
+
+console.log(`AUTO RENDER COMPLETE!`.green);
+
+function ErrorExit(){
+  console.log(`Failed to complete Minecraft Auto Overviewer schedule`.red)
+  console.log(`Exiting Minecraft Auto Overviewer...`.yellow)
   return;
 }
-console.log('Render complete!'.green);
-console.log('Copying new assets'.yellow);
-//shell.exec('./cp_new_assets.sh');
-shell.cp(`./assets/compass_upper-left.png`,`${config.render_out_dir}`)
-shell.cp(`./assets/index.html`, `${config.render_out_dir}`)
-shell.cp(`./assets/favicon.png`, `${config.render_out_dir}`)
-console.log('New Assets Copied'.green);
-console.log(`AUTO RENDER COMPLETE!`.green);
--- index.js
+++ index.js
@@ -8,14 +8,37 @@ const cron_patern = `${config.min} ${config.hour} ${config.dayom} ${config.month
 var interval = parser.parseExpression(cron_patern);
 
 console.log(`Minecraft Auto Overviewer Ready!`.green + ` Next Render scheduled for ${interval.next().toString()}`.yellow);
-var job = new CronJob(cron_patern , function() {
+var job = new CronJob(cron_patern, function() {
+
     console.log('New Render Started!'.green);
-    shell.exec(`python${config.python_ver} ${config.minecraft_overviewer_loc} --config=${config.minecraft_overviewer_config_loc}`);
-    console.log('Render complete!'.green);
+    var render_shell_out = shell.exec(`python${config.python_ver} ${config.minecraft_overviewer_loc} --config=${config.minecraft_overviewer_configfile_loc}`);
+    var renderResult = (render_shell_out.substring(render_shell_out.length - 28, render_shell_out.length)).replace(/\s+/g, '');
+
+    if (renderResult != "openindex.htmltoviewit.") {
+        console.log(`Error Minecraft Overviewer Render Failed`.red);
+        ErrorExit();
+    } else {
+        console.log('Render complete!'.green);
+    };
+
     console.log('Copying new assets'.yellow);
-    shell.exec('./cp_new_assets.sh');
+    for (var i = 0; i < config.assets.length; i++) {
+        try {
+            shell.cp(`./assets/${assets[i]}`, `${config.render_out_dir}`);
+        } catch (error) {
+            console.log('Error while copying assets to render folder'.red);
+            ErrorExit();
+        };
+        console.log(` => ${config.assets[i]} copied to render folder.`.cyan);
+    }
     console.log('New Assets Copied'.green);
     console.log(`AUTO RENDER COMPLETE!`.green + ` Last Render: ${interval.prev().toString()}`.cyan);
-    console.log(`Next Render scheduled for ${interval.next().toString()}`.yellow)
+    console.log(`Next Render scheduled for ${interval.next().toString()}`.yellow);
 }, null, true, config.time_zone);
 job.start();
+
+function ErrorExit() {
+    console.log(`Failed to complete Minecraft Auto Overviewer schedule`.red);
+    console.log(`Exiting Minecraft Auto Overviewer...`.yellow);
+    return;
+};
